533a337f59a6debe6c42046d5c38659b49112283
Kenji Hagiwara <khagiwara@fal.jp>
2017-07-26 05:58:19 +0000

    bootstrap designe Final2

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,7 +5,7 @@
 
 <div id="container">
    <div class="row">
-   <div class=" col-md-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+   <div class=" col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     <h1>メッセージ新規作成ページ</h1>
   {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         <div class="form-group">
